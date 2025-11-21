9028fae70553778ed26cb9c2d76cc06bb2a11750
Jared Cubias <jaredacubias@gmail.com>
2025-11-21 09:14:58 -0800

    Added time elapsed and recursion implementation

--- main.cpp
+++ main.cpp
@@ -3,15 +3,44 @@
 #include "bigint.h"
 
 #include <iostream>
-#include <time.h>
+#include <chrono>
+
+BigInt fiblinear(int n);
+BigInt fibrecursion(int n);
 
 int main() {
-    auto fibm2x2 = m2x2(BigInt("0"), BigInt("1"), BigInt("1"), BigInt("1"));
+    
     std::cout << "Enter a number for the n-th fibonacci number you want to find: ";
     int n;
     std::cin >> n;
+    auto start = std::chrono::high_resolution_clock::now();
+    BigInt resultlinear = fiblinear(n);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+    long long timeElapsed = duration.count();
+    std::cout << "The " << n << "-th fibonacci number is " << resultlinear << '\n';
+    std::cout << "Time elasped: " << timeElapsed << " nanoseconds.\n";
+    start = std::chrono::high_resolution_clock::now();
+    BigInt resultrecursion = fibrecursion(n);
+    end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+    timeElapsed = duration.count();
+    std::cout << "The " << n << "-th fibonacci number is " << resultrecursion << '\n';
+    std::cout << "Time elasped: " << timeElapsed << " nanoseconds.\n";
+}
+
+BigInt fiblinear(int n) {
+    auto fibm2x2 = m2x2(BigInt("0"), BigInt("1"), BigInt("1"), BigInt("1"));
     fibm2x2 = fibm2x2 ^ n; 
     auto fibv = vec2(BigInt("0"), BigInt("1"));
     auto ans = fibm2x2 * fibv;
-    std::cout << "The " << n << "-th fibonacci number is " << ans.x() << '\n';
+    return ans.x();
+}
+
+BigInt fibrecursion(int n) {
+    if (n == 1 || n == 2)
+    {
+        return BigInt("1");
+    }
+    return fibrecursion(n - 2) + fibrecursion(n - 1);
 }
